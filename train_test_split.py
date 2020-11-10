07f8dda3ec2d07a5a2ec469c454a7d876de0c63c
thihaaungycc <68723721+thihaaungycc@users.noreply.github.com>
2020-11-10 17:24:03 +0630

    Add files via upload

---
+++ train_test_split.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 10 15:55:03 2020
+
+@author: thiha
+"""
+from random import seed
+from random import randrange
+
+def train_test_split(dataset, split=0.60):
+    train = list()
+    train_size = split * len(dataset)
+    dataset_copy = list(dataset)
+    while len(train) < train_size:
+        index = randrange(len(dataset_copy))
+        train.append(dataset_copy.pop(index))
+    return train, dataset_copy
+
+def cross_validation_split(dataset, folds=3):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / folds)
+    for i in range(folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+return dataset_split
